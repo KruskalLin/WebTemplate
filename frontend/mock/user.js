6dc2bf7f1e14904dfdfcfdf53b74ef9088f02592
LimKruscal <161250070@smail.nju.edu.cn>
2019-11-17 20:33:18 +0800

    initial frontend...

---
+++ frontend/mock/user.js
@@ -0,0 +1,96 @@
+import Mock from 'mockjs'
+
+const tokens = {
+  admin: {
+    token: 'admin-token'
+  },
+  user: {
+    token: 'user-token'
+  }
+}
+
+const users = {
+  'admin-token': {
+    roles: ['ROLE_ADMIN'],
+    name: 'admin'
+  },
+  'user-token': {
+    roles: ['ROLE_USER'],
+    name: 'user'
+  }
+}
+
+const data = Mock.mock({
+  'items|30': [{
+    id: '@id',
+    title: '@sentence(10, 20)',
+    'status|1': ['published', 'draft', 'deleted'],
+    author: 'name',
+    display_time: '@datetime',
+    pageviews: '@integer(300, 5000)'
+  }]
+})
+
+export default [
+  // user login
+  {
+    url: '/signin',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (!token) {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  },
+
+  // get user info
+  {
+    url: '/user/info\.*',
+    type: 'get',
+    response: config => {
+      const { token } = config.query
+      const info = users[token]
+
+      // mock error
+      if (!info) {
+        return {
+          code: 50008,
+          message: 'Login failed, unable to get user details.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: info
+      }
+    }
+  },
+
+  // user list
+  {
+    url: '/getUserList',
+    type: 'get',
+    response: config => {
+      const items = data.items
+      return {
+        code: 20000,
+        data: {
+          total: items.length,
+          items: items
+        }
+      }
+    }
+  }
+]
